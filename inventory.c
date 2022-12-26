1c54711fcae5108694e39fdba9618cb0b3a1e291
Nerijus Vilčinskas <nerijus.vilcinskas@gmail.com>
2022-12-26 01:46:15 +0000

    Start implementing inventory, chapter 5, still not finished, code broken

---
+++ inventory.c
@@ -0,0 +1,37 @@
+#include "object.h"
+#include "misc.h"
+#include "noun.h"
+#include "move.h"
+
+void executeGet(const char* noun) {
+
+    Object* obj = getVisible("what you want to get", noun);
+    if (obj == NULL) {
+    } else if (obj == player) {
+        printf("You should not do that to yourself.\n");
+    } else if (obj->location == player) {
+        printf("You already have %s in your bag.\n", obj->description);
+    } else if (obj->location == guard) {
+        printf("You should ask %s nicely.\n", obj->location->description)
+    } else {
+        moveObject(obj, player);
+    }
+}
+
+void executeDrop(const char* noun) {
+    moveObject(getPossesion(player, "drop", noun), player->location);
+}
+
+void executeAsk(const char* noun) {
+    moveObject(getPossesion(actorHere(), "ask", noun), player);
+}
+
+void executeGive(const char* noun) {
+    moveObject(getPossesion(player, "drop", noun), actorHere());
+}
+
+void executeInventory(void) {
+    if (listObjectsAtLocation(player) == 0) {
+        printf("You are poor as heck, your pockets are empty.\n");
+    }
+}
--- parsexec.c
+++ parsexec.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "location.h"
+#include "inventory.h"
 
 bool parseAndExecute(char* input) {
     char* verb = strtok(input, " \n");
@@ -16,6 +17,16 @@ bool parseAndExecute(char* input) {
         } else if (strcmp(verb, "go") == 0) {
             executeGo(noun);
             /*printf("It is too dark to go anywhere.\n");*/
+        } else if (strcmp(verb, "get") == 0) {
+            executeGet(noun);
+        } else if (strcmp(verb, "drop") == 0) {
+            executeDrop(noun);
+        } else if (strcmp(verb, "give") == 0) {
+            executeGive(noun);
+        } else if (strcmp(verb, "ask") == 0) {
+            executeAsk(noun);
+        } else if (strcmp(verb, "inventory") == 0) {
+            executeInventory();
         } else {
             printf("How do you %s?\n", verb);
         }
